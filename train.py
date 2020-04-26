a8c415497fead96ff2b023dec4659a63b9369a67
yt7589 <yt7589@qq.com>
2020-04-26 08:31:48 +0800

    restore the default momentum value

--- train.py
+++ train.py
@@ -224,7 +224,7 @@ if __name__ == '__main__':
 
     lr_ratio = args.cls_lr_ratio
     base_lr = args.base_lr
-    momentum = 0.6 # 0.9
+    momentum = 0.9
     if Config.use_backbone:
         optimizer = optim.SGD([{'params': base_params},
                                {'params': model.module.classifier.parameters(), 'lr': base_lr}], lr = base_lr, momentum=momentum)
