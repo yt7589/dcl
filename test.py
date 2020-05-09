f2a41dda24dc6dcbf89d0288e0bf943c7cc35ac6
yt7589 <yt7589@qq.com>
2020-05-09 20:57:18 +0800

    try to load model by train.py code

--- test.py
+++ test.py
@@ -1,57 +1,82 @@
 #coding=utf-8
 import os
-import json
-import csv
+import sys
+import datetime
 import argparse
-import pandas as pd
+import logging
 import numpy as np
-from math import ceil
-from tqdm import tqdm
-import pickle
-import shutil
+import pandas as pd
 
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
-from torch.nn import CrossEntropyLoss
+from  torch.nn import CrossEntropyLoss
+import torch.utils.data as torchdata
 from torchvision import datasets, models
+import torch.optim as optim
+from torch.optim import lr_scheduler
 import torch.backends.cudnn as cudnn
-import torch.nn.functional as F
-import matplotlib.pyplot as plt
+from torchsummary import summary
+import torch.onnx as onnx
+from torch.onnx import OperatorExportTypes
 
 from transforms import transforms
+from utils.train_model import train
+from utils.train_model import log_progress
 from models.LoadModel import MainModel
-from utils.dataset_DCL import collate_fn4train, collate_fn4test, collate_fn4val, dataset
 from config import LoadConfig, load_data_transformers
-from utils.test_tool import set_text, save_multi_img, cls_base_acc
+import utils.dataset_DCL as dclds
+from utils.dataset_DCL import collate_fn4train, collate_fn4val, collate_fn4test, collate_fn4backbone, dataset
 
 import pdb
-import cv2
+import utils.utils as du
 
 os.environ['CUDA_DEVICE_ORDRE'] = 'PCI_BUS_ID'
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+#torch.backends.cudnn.benchmark = False
 
+# parameters setting
 def parse_args():
     parser = argparse.ArgumentParser(description='dcl parameters')
     parser.add_argument('--data', dest='dataset',
                         default='CUB', type=str)
+    parser.add_argument('--save', dest='resume',
+                        default=None,
+                        type=str)
     parser.add_argument('--backbone', dest='backbone',
                         default='resnet50', type=str)
-    parser.add_argument('--b', dest='batch_size',
-                        default=1, type=int)
-    parser.add_argument('--nw', dest='num_workers',
-                        default=1, type=int)
-    parser.add_argument('--ver', dest='version',
-                        default='val', type=str)
-    parser.add_argument('--save', dest='resume',
-                        default=None, type=str)
+    parser.add_argument('--auto_resume', dest='auto_resume',
+                        action='store_true')
+    parser.add_argument('--epoch', dest='epoch',
+                        default=360, type=int)
+    parser.add_argument('--tb', dest='train_batch',
+                        default=8, type=int)
+    parser.add_argument('--vb', dest='val_batch',
+                        default=512, type=int)
+    parser.add_argument('--sp', dest='save_point',
+                        default=5000, type=int)
+    parser.add_argument('--cp', dest='check_point',
+                        default=5000, type=int)
+    parser.add_argument('--lr', dest='base_lr',
+                        default=0.0008, type=float)
+    parser.add_argument('--lr_step', dest='decay_step',
+                        default=60, type=int)
+    parser.add_argument('--cls_lr_ratio', dest='cls_lr_ratio',
+                        default=10.0, type=float)
+    parser.add_argument('--start_epoch', dest='start_epoch',
+                        default=0,  type=int)
+    parser.add_argument('--tnw', dest='train_num_workers',
+                        default=16, type=int)
+    parser.add_argument('--vnw', dest='val_num_workers',
+                        default=32, type=int)
+    parser.add_argument('--detail', dest='discribe',
+                        default='', type=str)
     parser.add_argument('--size', dest='resize_resolution',
                         default=512, type=int)
     parser.add_argument('--crop', dest='crop_resolution',
                         default=448, type=int)
-    parser.add_argument('--ss', dest='save_suffix',
-                        default=None, type=str)
-    parser.add_argument('--acc_report', dest='acc_report',
+    parser.add_argument('--cls_2', dest='cls_2',
+                        action='store_true')
+    parser.add_argument('--cls_mul', dest='cls_mul',
                         action='store_true')
     parser.add_argument('--swap_num', default=[7, 7],
                     nargs=2, metavar=('swap1', 'swap2'),
@@ -59,118 +84,82 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-
-def demo(sess, net, image_name):
-    """Detect object classes in an image using pre-computed object proposals."""
- 
-    # Load the demo image
-    im_file = os.path.join(cfg.DATA_DIR, 'demo', image_name)
-    im = cv2.imread(im_file)
- 
-    # Detect all object classes and regress object bounds
-    timer = Timer()
-    timer.tic()
-    scores, boxes = im_detect(sess, net, im)
-    timer.toc()
-    print('Detection took {:.3f}s for {:d} object proposals'.format(timer.total_time, boxes.shape[0]))
- 
-    # Visualize detections for each class
-
+def auto_load_resume(load_dir):
+    folders = os.listdir(load_dir)
+    #date_list = [int(x.split('_')[1].replace(' ',0)) for x in folders]
+    date_list = [int(x.split('_')[2]) for x in folders]
+    choosed = folders[date_list.index(max(date_list))]
+    weight_list = os.listdir(os.path.join(load_dir, choosed))
+    acc_list = [x[:-4].split('_')[-1] if x[:7]=='weights' else 0 for x in weight_list]
+    acc_list = [float(x) for x in acc_list]
+    choosed_w = weight_list[acc_list.index(max(acc_list))]
+    return os.path.join(load_dir, choosed, choosed_w)
 
 if __name__ == '__main__':
     args = parse_args()
-    print(args)
-    #if args.submit:
-    if True:
-        args.version = 'test'
-        if args.save_suffix == '':
-            raise Exception('**** miss --ss save suffix is needed. ')
-
-    Config = LoadConfig(args, args.version)
+    print(args, flush=True)
+    Config = LoadConfig(args, 'train')
+    Config.cls_2 = args.cls_2
+    Config.cls_2xmul = args.cls_mul
+    assert Config.cls_2 ^ Config.cls_2xmul
     transformers = load_data_transformers(args.resize_resolution, args.crop_resolution, args.swap_num)
-    data_set = dataset(Config,\
-                       anno=Config.val_anno if args.version == 'val' else Config.test_anno ,\
-                       #unswap=transformers["None"],\
-                       swap=transformers["None"],\
-                       totensor=transformers['test_totensor'],\
-                       test=True)
-
-    dataloader = torch.utils.data.DataLoader(data_set,\
-                                             batch_size=args.batch_size,\
-                                             shuffle=False,\
-                                             num_workers=args.num_workers,\
-                                             collate_fn=collate_fn4test)
-
-    setattr(dataloader, 'total_item_len', len(data_set))
+    # inital dataloader
+    train_set = dataset(Config = Config,\
+                        anno = Config.train_anno,\
+                        common_aug = transformers["common_aug"],\
+                        swap = transformers["swap"],\
+                        totensor = transformers["train_totensor"],\
+                        train = True)
+    trainval_set = dataset(Config = Config,\
+                        anno = Config.train_anno,\
+                        common_aug = transformers["None"],\
+                        swap = transformers["None"],\
+                        totensor = transformers["val_totensor"],\
+                        train = False,
+                        train_val = True)
+    val_set = dataset(Config = Config,\
+                      anno = Config.val_anno,\
+                      common_aug = transformers["None"],\
+                      swap = transformers["None"],\
+                      totensor = transformers["test_totensor"],\
+                      test=True)
+    dataloader = {}
+    dataloader['train'] = torch.utils.data.DataLoader(train_set,\
+                                                batch_size=args.train_batch,\
+                                                shuffle=True,\
+                                                num_workers=args.train_num_workers,\
+                                                collate_fn=collate_fn4train if not Config.use_backbone else collate_fn4backbone,
+                                                drop_last=True if Config.use_backbone else False,
+                                                pin_memory=True)
+    setattr(dataloader['train'], 'total_item_len', len(train_set))
+    dataloader['trainval'] = torch.utils.data.DataLoader(trainval_set,\
+                                                batch_size=args.val_batch,\
+                                                shuffle=False,\
+                                                num_workers=args.val_num_workers,\
+                                                collate_fn=collate_fn4val if not Config.use_backbone else collate_fn4backbone,
+                                                drop_last=True if Config.use_backbone else False,
+                                                pin_memory=True)
+    setattr(dataloader['trainval'], 'total_item_len', len(trainval_set))
+    setattr(dataloader['trainval'], 'num_cls', Config.numcls)
+    dataloader['val'] = torch.utils.data.DataLoader(val_set,\
+                                                batch_size=args.val_batch,\
+                                                shuffle=False,\
+                                                num_workers=args.val_num_workers,\
+                                                collate_fn=collate_fn4test if not Config.use_backbone else collate_fn4backbone,
+                                                drop_last=True if Config.use_backbone else False,
+                                                pin_memory=True)
+    setattr(dataloader['val'], 'total_item_len', len(val_set))
+    setattr(dataloader['val'], 'num_cls', Config.numcls)
     cudnn.benchmark = True
-    resume = './net_model/training_descibe_5910_CUB/weights_11_2144_0.9894_0.9980.pth'
+    print('Choose model and train set', flush=True)
     model = MainModel(Config)
-    model_dict=model.state_dict()
-    pretrained_dict=torch.load(resume)
+    resume = './net_model/training_descibe_5910_CUB/weights_11_2144_0.9894_0.9980.pth'
+    model_dict = model.state_dict()
+    pretrained_dict = torch.load(resume)
     pretrained_dict = {k[7:]: v for k, v in pretrained_dict.items() if k[7:] in model_dict}
     model_dict.update(pretrained_dict)
     model.load_state_dict(model_dict)
     model.cuda()
     model = nn.DataParallel(model)
-    model.train(False)
-    with torch.no_grad():
-        val_corrects1 = 0
-        val_corrects2 = 0
-        val_corrects3 = 0
-        val_size = ceil(len(data_set) / dataloader.batch_size)
-        result_gather = {}
-        count_bar = tqdm(total=dataloader.__len__())
-        for batch_cnt_val, data_val in enumerate(dataloader):
-            count_bar.update(1)
-            inputs, labels, img_name = data_val
-            inputs = Variable(inputs.cuda())
-            labels = Variable(torch.from_numpy(np.array(labels)).long().cuda())
-            outputs = model(inputs)
-            outputs_pred = outputs[0] + outputs[1][:,0:Config.numcls] + outputs[1][:,Config.numcls:2*Config.numcls]
-            top3_val, top3_pos = torch.topk(outputs_pred, 3)
-            print('top3_val: {0};'.format(top3_val))
-            print('top3_pos: {0};'.format(top3_pos))
-            #print(top3_val)  
-            #tensor([[57.0976, 37.0089, 30.7548]], device='cuda:0')         
-            #print(top3_pos[:, 0])            
-            #tensor([477], device='cuda:0')
-            #print(top3_pos[:, 1])
-            #tensor([390], device='cuda:0')
-            #print(top3_pos[:, 2])            
-            #tensor([228], device='cuda:0')
-
-            if args.version == 'val':
-                batch_corrects1 = torch.sum((top3_pos[:, 0] == labels)).data.item()
-                val_corrects1 += batch_corrects1
-                batch_corrects2 = torch.sum((top3_pos[:, 1] == labels)).data.item()
-                val_corrects2 += (batch_corrects2 + batch_corrects1)
-                batch_corrects3 = torch.sum((top3_pos[:, 2] == labels)).data.item()
-                val_corrects3 += (batch_corrects3 + batch_corrects2 + batch_corrects1)
-            if args.acc_report:
-                for sub_name, sub_cat, sub_val, sub_label in zip(img_name, top3_pos.tolist(), top3_val.tolist(), labels.tolist()):
-                    result_gather[sub_name] = {'top1_cat': sub_cat[0], 'top2_cat': sub_cat[1], 'top3_cat': sub_cat[2],
-                                               'top1_val': sub_val[0], 'top2_val': sub_val[1], 'top3_val': sub_val[2],
-                                               'label': sub_label}
-    if args.acc_report:
-        torch.save(result_gather, 'result_gather_%s'%resume.split('/')[-1][:-4]+ '.pt')
-
-    count_bar.close()
-
-    if args.acc_report:
-
-        val_acc1 = val_corrects1 / len(data_set)
-        val_acc2 = val_corrects2 / len(data_set)
-        val_acc3 = val_corrects3 / len(data_set)
-        print('%sacc1 %f%s\n%sacc2 %f%s\n%sacc3 %f%s\n'%(8*'-', val_acc1, 8*'-', 8*'-', val_acc2, 8*'-', 8*'-',  val_acc3, 8*'-'))
-
-        cls_top1, cls_top3, cls_count = cls_base_acc(result_gather)
-
-        acc_report_io = open('acc_report_%s_%s.json'%(args.save_suffix, resume.split('/')[-1]), 'w')
-        json.dump({'val_acc1':val_acc1,
-                   'val_acc2':val_acc2,
-                   'val_acc3':val_acc3,
-                   'cls_top1':cls_top1,
-                   'cls_top3':cls_top3,
-                   'cls_count':cls_count}, acc_report_io)
-        acc_report_io.close()
+    print('^_^ The End Load model is success!')
 
