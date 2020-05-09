d22ee99c2c49e28bb5262658bead525190c2fdb1
yt7589 <yt7589@qq.com>
2020-05-09 23:32:55 +0800

    fix tqdm bug

--- test.py
+++ test.py
@@ -137,5 +137,6 @@ if __name__ == '__main__':
             outputs = model(inputs)
             y_hat = torch.argmax(outputs[0], dim=1)
             val_corrects1 += torch.sum(torch.eq(y_hat, labels).clone().detach().to(torch.uint8)).item()
+    count_bar.close()
     print('top1 accuracy: {0}={1}/{2};'.format( val_corrects1 / len(val_set), val_corrects1, len(val_set)))
 
