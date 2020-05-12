f00479e4dcecbe176c034a4df81532d1c712573c
yt7589 <yt7589@qq.com>
2020-05-12 10:02:36 +0800

    add more debug log

--- utils/data_preprocessor.py
+++ utils/data_preprocessor.py
@@ -211,7 +211,7 @@ class DataPreprocessor(object):
                 arrs0 = full_name.split('/')
                 arrs1 = arrs0[0].split('_')
                 vc = arrs1[0]
-                print('vc: {0};'.format(vc))
+                print('{0}: vc={1};'.format(full_name, vc))
                 if vc in vc_bmy:
                     bmy = vc_bmy[vc]
                     if not (bmy in bmy_set):
