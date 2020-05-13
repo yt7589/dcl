c21c1bc526f36bbc6aa9903044f3f5ec0b1aefd4
yt7589 <yt7589@qq.com>
2020-05-13 18:14:15 +0800

    use swqp1 and swap2 command line args

--- train.py
+++ train.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
                         anno = Config.train_anno,\
                         common_aug = transformers["common_aug"],\
                         swap = transformers["swap"],\
-                        swap_size=swap_num, \
+                        swap_size=[swap1, swap2], \
                         totensor = transformers["train_totensor"],\
                         train = True)
 
@@ -136,7 +136,7 @@ if __name__ == '__main__':
                         anno = Config.train_anno,\
                         common_aug = transformers["None"],\
                         swap = transformers["None"],\
-                        swap_size=swap_num, \
+                        swap_size=[swap1, swap2], \
                         totensor = transformers["val_totensor"],\
                         train = False,
                         train_val = True)
@@ -145,7 +145,7 @@ if __name__ == '__main__':
                       anno = Config.val_anno,\
                       common_aug = transformers["None"],\
                       swap = transformers["None"],\
-                        swap_size=swap_num, \
+                        swap_size=[swap1, swap2], \
                       totensor = transformers["test_totensor"],\
                       test=True)
 
