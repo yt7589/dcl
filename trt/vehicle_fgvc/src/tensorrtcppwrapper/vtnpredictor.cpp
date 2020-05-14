30efadd07d6b1bb93d10b937105620b394a3f5a8
yt7589 <yt7589@qq.com>
2020-05-14 21:41:53 +0800

    print the raw network output format

--- trt/vehicle_fgvc/src/tensorrtcppwrapper/vtnpredictor.cpp
+++ trt/vehicle_fgvc/src/tensorrtcppwrapper/vtnpredictor.cpp
@@ -4,6 +4,7 @@
 
 void VTNPredictor::postProcess(const std::vector<cv::Mat> &images, std::vector<std::vector<float> >& net_outputs, std::vector<std::vector<float> >& out_results) const
 {
+	std::cout<<"raw output: picture_num="<<net_outputs.size()<<"; dim="<<net_outputs[0].size()<<";"<<std::endl;
 	for (int in = 0; in < net_outputs[0].size()/2; ++in)
 	{
 		double a = std::exp(net_outputs[0][2*in]);
