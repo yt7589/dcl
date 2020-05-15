b6dec5a666ed4e1428563827eb77872d5f19fc2e
yantao <yt7589@qq.com>
2020-05-15 11:48:46 +0800

    fix BUG20200515010

--- trt/vehicle_fgvc/src/tensorrtcppwrapper/vtnpredictor.cpp
+++ trt/vehicle_fgvc/src/tensorrtcppwrapper/vtnpredictor.cpp
@@ -8,47 +8,22 @@ const int IMG_H = 224;
 
 void VTNPredictor::postProcess(const std::vector<cv::Mat> &images, std::vector<std::vector<float> >& net_outputs, std::vector<std::vector<float> >& out_results) const
 {
-	std::cout<<"net_outputs: picture_num="<<net_outputs.size()<<"; dim="<<net_outputs[0].size()<<";"<<std::endl;
 	// 除以最小批次
 	int batchsize = 8;
 	int classNum = net_outputs[0].size()/batchsize;
-	std::cout<<"v="<<net_outputs[0][179]<<";"<<std::endl;
 	for (int in = 0; in < batchsize; ++in)
 	{
 		std::vector<float> rst;
 		for (int j=0; j<classNum; j++)
 		{
-			std::cout<<in<<": j="<<j<<"; idx="<<in*classNum + j<<"; val="<<net_outputs[0][in*classNum + j]<<";"<<std::endl;
 			rst.push_back(net_outputs[0][in*classNum + j]);
 		}
-		std::cout<<"rst.size:"<<rst.size()<<"; out_results.size:"<<out_results.size()<<"; s="<<out_results[0].size()<<";"<<std::endl;
-		//double a = std::exp(net_outputs[0][2*in]);
-		//double b = std::exp(net_outputs[0][1+2*in]);
 		out_results.push_back(rst);
 	}	
-	
-
-
-
 	for (auto &item:out_results)
 	{
 		item.pop_back();
 	}
-	//return;
-		
-//	for (auto item:out_results)
-//	{
-//		for (auto inner_item:item)
-//		{
-//		std::cout << inner_item << " " ;
-//		}
-//		std::cout << std::endl;
-//	}
-
-
-
-	
-	
 }
 
 void VTNPredictor::postProcess(float* pgpu,int num,
