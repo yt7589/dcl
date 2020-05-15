d3b6f396e95975cb5c228906f4a9f19b50395e60
yantao <yt7589@qq.com>
2020-05-15 11:51:08 +0800

    remove the unneed files

--- run.sh
+++
@@ -1,1 +0,0 @@
-python train.py --data CUB --epoch 360 --backbone resnet50 --cp 100 --sp 100 --tb 32 --tnw 16 --vb 256 --vnw 16  --lr 0.0008 --lr_step 50 --cls_lr_ratio 10 --start_epoch 0 --detail training_descibe --size 224 --crop 224 --cls_2 --swap_num 3 3
