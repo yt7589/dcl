ddcb13ae188c47e07b26c23bb42c1debccd0eceb
yt7589 <yt7589@qq.com>
2020-05-19 17:06:37 +0800

    prepare the cluster test images

--- apps/cluster/cluster_app.py
+++ apps/cluster/cluster_app.py
@@ -1,4 +1,5 @@
 # 图像聚类主程序
+import shutil
 from apps.cluster.image_dbscan import ImageDbscan
 
 class ClusterApp(object):
@@ -6,5 +7,17 @@ class ClusterApp(object):
         self.name = 'apps.cluster.ClusterApp'
 
     def startup(self):
-        engine = ImageDbscan()
-        engine.run()
+        self.prepare_test_images()
+        #engine = ImageDbscan()
+        #engine.run()
+
+    def prepare_test_images(self):
+        '''
+        将训练数据集文件中指定的图片，以a+类别编号形式命名并拷贝文件
+        '''
+        with open('./datasets/CUB_200_2011/anno/t1.txt', 'r', encoding='utf-8') as fd:
+            for line in fd:
+                arrs0 = line.split('*')
+                src_img = arrs0[0]
+                fgvc_id = arrs0[-1][:-1]
+                shutil.copy(src_img, './work/a{0}.jpg'.format(fgvc_id))
