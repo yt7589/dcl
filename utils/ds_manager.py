390415e4539e343c09a2c1dd9aeb387765dcf332
yt7589 <yt7589@qq.com>
2020-05-21 21:11:36 +0800

    add -n at the end of the line

--- utils/ds_manager.py
+++ utils/ds_manager.py
@@ -471,7 +471,7 @@ class DsManager(object):
                 gbd_fd.write('{0}:{1}\n'.format(k, v))
         with open('./work/unknown_ggh.txt', 'w+', encoding='utf-8') as ub_fd:
             for ggh in unknown_ggh_set:
-                ub_fd.write('{0}'.format(ggh))
+                ub_fd.write('{0}\n'.format(ggh))
 
     @staticmethod
     def process_interm_txt_file(txt_file, ggh_set, unknown_ggh_set, bmy_set, ggh_to_bmy_dict):
