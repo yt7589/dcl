f1577354dba220c445716605c9af75ed3e0c72a7
yt7589 <yt7589@qq.com>
2020-05-22 16:36:14 +0800

    generate the dataset

--- utils/ds_manager.py
+++ utils/ds_manager.py
@@ -37,7 +37,7 @@ class DsManager(object):
 
     @staticmethod
     def startup():
-        run_mode = DsManager.RUN_MODE_MERGE_BMY_FGVC_ID
+        run_mode = DsManager.RUN_MODE_GENERATE_FGVC_DS
         # refine_bmy_and_fgvc_id_dicts
         if DsManager.RUN_MODE_SAMPLE_IMPORTED == run_mode:
             # 从进口车目录随机选取数据
