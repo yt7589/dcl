db4128fd46355d0158aad7dfe19ba28cd4560d30
yt7589 <yt7589@qq.com>
2020-05-29 20:26:19 +0800

    test insert_one of mongodb

--- apps/vbg/model/m_vehicle_brand.py
+++ apps/vbg/model/m_vehicle_brand.py
@@ -1,8 +1,12 @@
 #
+import pymongo
 
 class MVehicleBrand(object):
     def __init__(self):
         self.name = 'apps.model.MVehicleBrand'
+        mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
+        self.mongo_db = mongo_client['vehicle_brands']
 
     def insert_vehicle_brand(self, vehicle_brand_vo):
-        pass
+        rst = self.mongo_db.insert_one(vehicle_brand_vo)
+        print('insert_id={0};'.format(rst.inserted_id))
--- apps/vbg/vbg_app.py
+++ apps/vbg/vbg_app.py
@@ -1,4 +1,5 @@
 # 车标识别游戏
+from apps.vbg.model.m_vehicle_brand import MVehicleBrand
 
 class VbgApp(object):
     def __init__(self):
@@ -6,3 +7,11 @@ class VbgApp(object):
 
     def startup(self):
         print('车标游戏')
+        model = MVehicleBrand()
+        vehicle_brand_vo = {
+            'vehicle_brand_id': 2,
+            'vehicle_brand_name': '雪铁龙',
+            'vehicle_brand_alias': 'CITROEN',
+            'place_of_origin': '法国·巴黎'
+        }
+        model.insert_vehicle_brand(vehicle_brand_vo)
