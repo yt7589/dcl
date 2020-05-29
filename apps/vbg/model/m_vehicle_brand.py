6fb20edc912a9c863ca0699bdb150ea9c703c062
yt7589 <yt7589@qq.com>
2020-05-29 20:29:07 +0800

    operate on mongodb collection

--- apps/vbg/model/m_vehicle_brand.py
+++ apps/vbg/model/m_vehicle_brand.py
@@ -5,8 +5,9 @@ class MVehicleBrand(object):
     def __init__(self):
         self.name = 'apps.model.MVehicleBrand'
         mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
-        self.mongo_db = mongo_client['vehicle_brands']
+        self.mongo_db = mongo_client['tcvdb']
 
     def insert_vehicle_brand(self, vehicle_brand_vo):
-        rst = self.mongo_db.insert_one(vehicle_brand_vo)
+        tbl = self.mongo_db['vehicle_brands']
+        rst = tbl.insert_one(vehicle_brand_vo)
         print('insert_id={0};'.format(rst.inserted_id))
