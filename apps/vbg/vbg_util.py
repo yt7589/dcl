6e68c904abe1d6cb405d10b8e76c8152eeb17c95
yt7589 <yt7589@qq.com>
2020-05-30 16:08:36 +0800

    download vehicle brand name and icons from chebiao website

--- apps/vbg/vbg_util.py
+++ apps/vbg/vbg_util.py
@@ -3,6 +3,8 @@ import json
 import urllib
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+#
+from apps.vbg.model.m_vehicle_brand import MVehicleBrand
 
 class VbgUtil(object):
     @staticmethod
@@ -15,7 +17,6 @@ class VbgUtil(object):
         vehicle_brand_id = 1
         for idx in range(1, len(items)):
             vehicle_brand_vo = {'vehicle_brand_id': vehicle_brand_id}
-            vehicle_brand_id += 1
             img = items[idx].contents[1].contents[0]
             vehicle_brand_vo['vehicle_brand_name'] = img['alt'][:-2]
             vehicle_brand_vo['vbicon'] = img['src']
@@ -23,7 +24,12 @@ class VbgUtil(object):
             arrs0 = notes.split('　')
             vehicle_brand_vo['vehicle_brand_alias'] = arrs0[0][6:]
             vehicle_brand_vo['place_of_origin'] = arrs0[1][3:]
-            print('{0}; {1:03d}'.format(json.dumps(vehicle_brand_vo, ensure_ascii=False), vehicle_brand_id))
+            #print('{0}; {1:03d}'.format(json.dumps(vehicle_brand_vo, ensure_ascii=False), vehicle_brand_id))
+            MVehicleBrand.insert_vehicle_brand(vehicle_brand_vo)
+            VbgUtil.download_image(vehicle_brand_vo['vbicon'], 
+                        '/media/zjkj/35196947-b671-441e-9631-6245942d671b/'
+                        'yantao/web_root/images/vbg/vbicon_{0:03d}.jpg'.format(vehicle_brand_id))
+            vehicle_brand_id += 1
 
     @staticmethod
     def download_image(img_url, img_file):
