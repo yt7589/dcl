cb249ad945724c17fc09eb85e880d7b3848cc045
yt7589 <yt7589@qq.com>
2020-05-30 18:29:09 +0800

    create a question for survey

---
+++ apps/vbg/controller/c_vbg.py
@@ -0,0 +1,39 @@
+#
+from apps.vbg.model.m_vehicle_brand import MVehicleBrand
+
+class CVbg(object):
+    @staticmethod
+    def get_survey_data(question_num):
+        model = MVehicleBrand()
+        total = model.get_total_recs()
+        vehicle_brand_ids = set()
+        survey = []
+        question = Cvbg.create_question(total, vehicle_brand_ids)
+        print(question)
+        survey.append(question)
+
+    @staticmethod
+    def create_question(total, vehicle_brand_ids):
+        question = {}
+        rec = model.get_random_rec(total)
+        while rec['vehicle_brand_id'] in vehicle_brand_ids:
+            rec = model.get_random_rec(total)
+        vehicle_brand_ids.add(rec['vehicle_brand_id'])
+        question['vbicon'] = 'displayVbicon/vbicon_{0}.jpg'.format(rec['vehicle_brand_id'])
+        question['answer'] = rec['vehicle_brand_id']
+        question['choose'] = -1
+        pos = random.randint(1, 4)
+        options = []
+        for idx in range(1, pos):
+            opt = model.get_random_rec(total)
+            while opt['vehicle_brand_id'] == rec['vehicle_brand_id']:
+                opt = model.get_random_rec(total)
+            options.append(opt)
+        options.append(rec)
+        for idx in range(pos+1, 4+1):
+            opt = model.get_random_rec(total)
+            while opt['vehicle_brand_id'] == rec['vehicle_brand_id']:
+                opt = model.get_random_rec(total)
+            options.append(opt)
+        question['options'] = options
+        return question
--- apps/vbg/vbg_app.py
+++ apps/vbg/vbg_app.py
@@ -1,4 +1,5 @@
 # 车标识别游戏
+from apps.vbg.controller.c_vbg import CVbg
 from apps.vbg.model.m_vehicle_brand import MVehicleBrand
 from apps.vbg.model.m_file_odb import MFileOdb
 
@@ -8,8 +9,5 @@ class VbgApp(object):
 
     def startup(self):
         print('车标游戏')
-        model = MVehicleBrand()
-        total = model.get_total_recs()
-        print('count={0};'.format(total))
-        rec = model.get_random_rec(total)
-        print('^_^ rec: {0};'.format(rec))
+        CVbg.get_survey_data(50)
+        
