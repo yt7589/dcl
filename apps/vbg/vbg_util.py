94d90f3ec9f25cefde7cfa2b4bb1721d210a9db7
yt7589 <yt7589@qq.com>
2020-05-30 14:33:52 +0800

    display the children element of ul

--- apps/vbg/vbg_util.py
+++ apps/vbg/vbg_util.py
@@ -12,4 +12,6 @@ class VbgUtil(object):
         items = bs.find_all("ul")
         for item in items:
             print('{0}: {1};'.format(type(item), item))
-            print('img: {0}; {1}'.format(type(item.children), item.children[0]))
+            eles = item.contents()
+            for idx in range(len(eles)):
+                print('{0}: {1};'.format(idx, eles[idx]))
