94c0bca1b8acf124198b5739d39f45025be89862
yantao <yt7589@qq.com>
2020-05-30 15:54:14 +0800

    parse the web page of chebiao site

--- apps/vbg/vbg_util.py
+++ apps/vbg/vbg_util.py
@@ -1,4 +1,5 @@
 # 工具类：准备品牌及车标
+import json
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -10,8 +11,15 @@ class VbgUtil(object):
         ).read().decode('gb2312')
         bs = BeautifulSoup(html, "html.parser")
         items = bs.find_all("ul")
-        for item in items:
-            eles = item.contents
-            img = eles[1].contents[0]
-            for k, v in img.attrs.items():
-                print('[{0}]={1};'.format(k, v))
+        vehicle_brand_id = 1
+        for idx in range(1, len(items)):
+            vehicle_brand_vo = {'vehicle_brand_id': vehicle_brand_id}
+            vehicle_brand_id += 1
+            img = items[idx].contents[1].contents[0]
+            vehicle_brand_vo['vehicle_brand_name'] = img['alt'][:-2]
+            vehicle_brand_vo['vbicon'] = img['src']
+            notes = str(items[idx].contents[5])
+            arrs0 = notes.split('　')
+            vehicle_brand_vo['vehicle_brand_alias'] = arrs0[0][6:]
+            vehicle_brand_vo['place_of_origin'] = arrs0[1][3:]
+            print('{0}; {1:03d}'.format(json.dumps(vehicle_brand_vo, ensure_ascii=False), vehicle_brand_id))
