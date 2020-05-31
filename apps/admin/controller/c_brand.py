e06439de83d1f9c1ce885e729add3a99d33723e1
yt7589 <yt7589@qq.com>
2020-05-31 21:34:26 +0800

    display the final result

--- apps/admin/controller/c_brand.py
+++ apps/admin/controller/c_brand.py
@@ -33,11 +33,8 @@ class CBrand(object):
             model_str = str(model_path)
             arrs0 = model_str.split('/')
             model_name = arrs0[-1]
-            num += 1
             for year_path in model_path.iterdir():
-                num += 1
                 for file_obj in year_path.iterdir():
-                    print(file_obj)
                     num += 1
         return num
     
@@ -45,12 +42,10 @@ class CBrand(object):
     def get_known_brands(start_idx=1, amount=-1, sort_id=1, 
                 sort_type=1):
         print('获取已有品牌列表...')
-        num = CBrand.get_files_num_in_folder('/media/zjkj/35196947-b671-441e-9631-6245942d671b/fgvc_dataset/raw/奥迪')
-        print('file numbers: {0};'.format(num))
         #
-        '''
+        base_dir = '/media/zjkj/35196947-b671-441e-9631-6245942d671b/fgvc_dataset/raw'
         bns = set()
-        base_path = Path('/media/zjkj/35196947-b671-441e-9631-6245942d671b/fgvc_dataset/raw')
+        base_path = Path(base_dir)
         for brand_path in base_path.iterdir():
             brand_str = str(brand_path)
             arrs0 = brand_str.split('/')
@@ -58,8 +53,7 @@ class CBrand(object):
             bns.add(brand_name)
         brands = []
         for bn in bns:
+            num = CBrand.get_files_num_in_folder('{0}/{1}'.format(base_dir, bn))
             brand = {'name': bn, 'num': num}
             brands.append(brand)
         return sorted(brands, key=CBrand.sort_by_num, reverse=False)
-        '''
-        return []
