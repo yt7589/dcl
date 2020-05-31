d79b2e21fc58faf9c2faaf38d34d53d4ecb8c3ab
yt7589 <yt7589@qq.com>
2020-05-31 20:25:08 +0800

    merge test dataset to raw

--- apps/admin/controller/c_brand.py
+++ apps/admin/controller/c_brand.py
@@ -25,40 +25,14 @@ class CBrand(object):
                 sort_type=1):
         print('获取已有品牌列表...')
         #
-        raw_set1 = set()
+        brands = set()
         base_path = Path('/media/zjkj/35196947-b671-441e-9631-6245942d671b/fgvc_dataset/raw')
         for brand_path in base_path.iterdir():
             brand_str = str(brand_path)
             arrs0 = brand_str.split('/')
             brand_name = arrs0[-1]
-            raw_set1.add(brand_name)
-        print('raw_set1={0};'.format(len(raw_set1)))
-        #
-        raw_set2 = set()
-        base_path1 = Path('/media/zjkj/35196947-b671-441e-9631-6245942d671b/fgvc_dataset/t1')
-        for brand_path in base_path1.iterdir():
-            brand_str = str(brand_path)
-            arrs0 = brand_str.split('/')
-            brand_name = arrs0[-1]
-            raw_set2.add(brand_name)
-        print('raw_set2={0};'.format(len(raw_set2)))
-        #
-        novel_brands = set()
-        for brand in raw_set2:
-            if brand not in raw_set1:
-                novel_brands.add(brand)
-        print('novel brands: {0};'.format(len(novel_brands)))
-        #
-        print('拷贝新文件夹')
-        src_dir = '/media/zjkj/35196947-b671-441e-9631-6245942d671b/fgvc_dataset/t1'
-        dst_dir = '/media/zjkj/35196947-b671-441e-9631-6245942d671b/fgvc_dataset/t2'
-        for brand in novel_brands:
-            rst = os.popen('cp -R {0}/{1} {2}/.'.format(src_dir, brand, dst_dir))
-            print('{0}: {1};'.format(brand, rst))
-        brands = raw_set1
-        for brand in raw_set2:
-            brands.add(brand)
-        print('brands={0};'.format(len(brands)))
+            brands.add(brand_name)
+        print('v1 brands={0};'.format(len(brands)))
         rst = {
             'total': len(brands),
             'brands': brands
