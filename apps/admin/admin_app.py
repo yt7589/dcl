044c118f3bd22befaaefa3f8aba4c056b433bdc1
yt7589 <yt7589@qq.com>
2020-05-31 21:38:08 +0800

    fix BUG20200531011

--- apps/admin/admin_app.py
+++ apps/admin/admin_app.py
@@ -10,6 +10,6 @@ class AdminApp(object):
         print('后台管理程序调试工具类 v0.0.1')
         rst = CBrand.get_known_brands(start_idx=1, amount=-1, sort_id=1, 
                     sort_type=1)
-        print('已有品牌数量：{0};'.format(len(rst.brands)))
-        for brand_obj in rst.brands:
+        print('已有品牌数量：{0};'.format(len(rst)))
+        for brand_obj in rst:
             print('{0}: {1};'.format(brand_obj['name'], brand_obj['num']))
