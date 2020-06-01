48a690af91aded324a4fd87b3913289a894b77ca
yt7589 <yt7589@qq.com>
2020-06-01 21:36:47 +0800

    fix BUG20200601012

--- apps/admin/controller/c_bmy.py
+++ apps/admin/controller/c_bmy.py
@@ -33,23 +33,15 @@ class CBmy(object):
     def get_bmys_from_folder():
         base_path = Path('/media/zjkj/35196947-b671-441e-9631-6245942d671b/fgvc_dataset/raw')
         raw_bmys = []
-        stop_loop = False
-        sl_num = 0
         for brand_path in base_path.iterdir():
-            if stop_loop:
-                break
             brand_str = str(brand_path)
             arrs0 = brand_str.split('/')
             brand_name = arrs0[-1]
             for model_path in brand_path.iterdir():
-                if stop_loop:
-                    break
                 model_str = str(model_path)
                 arrs1 = model_str.split('/')
                 model_name = arrs1[-1]
                 for year_path in model_path.iterdir():
-                    if stop_loop:
-                        break
                     year_str = str(year_path)
                     arrs2 = year_str.split('/')
                     year_name = arrs2[-1]
@@ -59,16 +51,10 @@ class CBmy(object):
                     bmy_name = '{0}_{1}_{2}'.format(brand_name, model_name, year_name)
                     bmy = {'bmy_name': bmy_name, 'bmy_num': num}
                     raw_bmys.append(bmy)
-                    if sl_num > 10:
-                        stop_loop = True
-                    sl_num += 1
         bmys = []
-        for rb in raw_bmys:
-            print('### {0};'.format(rb))
         recs = sorted(raw_bmys, key=CBmy.sort_by_num_bmy, reverse=False)
         bmy_id = 1
         for rec in recs:
-            print('@@@ {0};'.format(rec))
             rec['bmy_id'] = bmy_id
             bmy = {
                 'bmy_id': rec['bmy_id'],
