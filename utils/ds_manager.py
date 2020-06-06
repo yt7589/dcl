32b873dbade7e5c20278336dd1b0c3f4543276bf
yt7589 <yt7589@qq.com>
2020-06-06 12:35:12 +0800

    fix BUG20200606004

--- utils/ds_manager.py
+++ utils/ds_manager.py
@@ -947,4 +947,4 @@ class DsManager(object):
             ug_rdr = csv.reader(ug_fd, delimiter=',')
             header = next(ug_rdr)
             for row in ug_rdr:
-                print('{0}:{1};'.format(type(row), row))
+                print('{0};'.format(type(row)))
