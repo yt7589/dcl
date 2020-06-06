77b7422dc2432ee0cd3165027f0330964449fd16
yt7589 <yt7589@qq.com>
2020-06-06 13:52:18 +0800

    display ggh and bmy in csv

--- utils/ds_manager.py
+++ utils/ds_manager.py
@@ -948,7 +948,9 @@ class DsManager(object):
             header = next(ug_rdr)
             sum = 0
             for row in ug_rdr:
-                print('{0}: {1}_{2}_{3}'.format(row[0].strip(), row[1].strip(), row[2].strip(), row[3].strip()))
+                ggh = row[0].strip()
+                bmy = '{0}_{1}_{2}'.format(row[1].strip(), row[2].strip(), row[3].strip())
+                print('{0}:{1};'.format(ggh, bmy))
                 if sum > 10:
                     break
                 sum += 1
