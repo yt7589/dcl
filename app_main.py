16198797031706c786c163827179fe82c9c011a7
yt7589 <yt7589@qq.com>
2020-06-09 21:05:34 +0800

    switch mode to save data to t_data_source

--- app_main.py
+++ app_main.py
@@ -59,7 +59,8 @@ def temp_func():
     VaoTest.draw_b86_train_curve()
 
 def test_web_api():
-    CDataSource.generate_delta_ds()
+    delta_ds_id = 6
+    CDeltaDs.save_to_dataset(delta_ds_id)
 
 def main(args):
     print('细粒度图像识别系统')
