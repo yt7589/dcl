1028047386360a1a7bda4eeb8dfea87feca2c7bd
yt7589 <yt7589@qq.com>
2020-06-11 17:46:40 +0800

    fix BUG20200611020

--- apps/admin/model/m_bmy_example.py
+++ apps/admin/model/m_bmy_example.py
@@ -11,16 +11,16 @@ class MBmyExample(object):
         
     @staticmethod
     def get_bmy_example_vehicle_image_id(bmy_id):
-        if MBmy.db is None:
-            MBmy._initialize()
+        if MBmyExample.db is None:
+            MBmyExample._initialize()
         query_cond = {'bmy_id': bmy_id}
         fields = {'vehicle_image_id': 1}
-        return MMongoDb.convert_rec(MBmy.tbl.find_one(query_cond, fields))
+        return MMongoDb.convert_rec(MBmyExample.tbl.find_one(query_cond, fields))
 
     @staticmethod
     def _initialize():
         mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
-        MBmy.db = mongo_client['tcvdb']
-        MBmy.tbl = MBmy.db['t_bmy']
+        MBmyExample.db = mongo_client['tcvdb']
+        MBmyExample.tbl = MBmyExample.db['t_bmy']
 
     
