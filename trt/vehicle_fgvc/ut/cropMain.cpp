046607251004ef40a29a93426eaeb39e2f48a478
yantao <yt7589@qq.com>
2020-06-24 19:44:57 +0800

    read file in c++

--- trt/vehicle_fgvc/ut/cropMain.cpp
+++ trt/vehicle_fgvc/ut/cropMain.cpp
@@ -93,22 +93,23 @@ int test001()
     
     szBuf = (char*)malloc(FBLOCK_MAX_BYTES * sizeof(char) + 1);
     cout<<"step 100"<<endl;
-    std::string strFileUTF8 = "/media/zjkj/35196947-b671-441e-9631-6245942d671b/vehicle_type_v2d/vehicle_type_v2d/datasets/CUB_200_2011/anno/test_ds_v4.txt";
+    std::string strFileUTF8 = "/media/zjkj/35196947-b671-441e-9631-6245942d671b/yantao/fgvc/dcl/datasets/CUB_200_2011/anno/test_ds_v4.txt";
     std::string strFileName = strFileUTF8;
     memset(szBuf, 0, sizeof(char) * FBLOCK_MAX_BYTES);
     std::string strMessage;
     FILE * fp = NULL;
+cout<<"step 2"<<strFileName.c_str()<<endl;
     fp = fopen(strFileName.c_str(), "rb");
+cout<<"step 3"<<fp<<endl;
     if (fp != NULL)
     {
-        std::cout<<"step 2"<<endl;
         // fseek(fp, sizeof(char) * 3, 0);
-        /*while(fread(szBuf, sizeof(char), FBLOCK_MAX_BYTES, fp) > 0)
+        while(fread(szBuf, sizeof(char), FBLOCK_MAX_BYTES, fp) > 0)
         {
             szBuf[FBLOCK_MAX_BYTES] = '\0';
             strMessage += szBuf;
             memset(szBuf, 0, sizeof(char) * FBLOCK_MAX_BYTES);
-        }*/
+        }
     }
     std::cout << strMessage << std::endl;
     fclose(fp);
