bb8cd45e2aad06c0b3af2f5e32c3005e0218b7fc
yt7589 <yt7589@qq.com>
2020-06-26 16:48:50 +0800

    fix BUG20200626010

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ from torch.onnx import OperatorExportTypes
 from transforms import transforms
 from utils.train_model import train, prepare_cluster_data
 from utils.train_model import log_progress
-from utils.eval_model import filter_samples, predict_main
+from utils.eval_model import filter_samples, predict_main, eval_turn
 from models.LoadModel import MainModel
 from config import LoadConfig, load_data_transformers
 import utils.dataset_DCL as dclds
