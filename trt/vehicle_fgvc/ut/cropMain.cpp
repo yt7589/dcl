1645849507b650f14975567ac5a412b70d6425f8
yt7589 <yt7589@qq.com>
2020-06-29 18:20:52 +0800

    display the raw content of the image

--- trt/vehicle_fgvc/ut/cropMain.cpp
+++ trt/vehicle_fgvc/ut/cropMain.cpp
@@ -84,6 +84,7 @@ std::tuple<std::vector<cv::Mat>, std::vector<int>> GetInputImage(vector<vector<s
         std::cout<<"img: "<<samples[startPos + t][0]<<"; classId: "<<samples[startPos + t][1]<<"; !!!!"<<std::endl;
         cv::Mat resized;
         cv::resize(img, resized, cv::Size(IMG_W, IMG_H), 0, 0);
+        std::cout<<"################"<<+static_cast<uint8_t>(resized.at<uint8_t>(0, 0))<<";"<<std::endl;
         inputs.push_back(resized.clone());
         results.push_back(std::stoi(samples[startPos + t][1]));
     }
