5ba49ca484b16172a79e84d719c6e9df7027bc8b
yt7589 <yt7589@qq.com>
2020-06-29 20:48:27 +0800

    fix BUG20200629005

--- trt/vehicle_fgvc/ut/cropMain.cpp
+++ trt/vehicle_fgvc/ut/cropMain.cpp
@@ -5,7 +5,7 @@
 #include <unistd.h>
 //common
 #include "file_util.hpp"
-//#include "opencv2/opencv.hpp"
+#include "opencv2/opencv.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
