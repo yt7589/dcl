59dd7ffb0851cd88f9c0c0ceaf3317ef2530a46f
yt7589 <yt7589@qq.com>
2020-06-30 10:10:17 +0800

    use flatten to reshape the x

--- models/LoadModel.py
+++ models/LoadModel.py
@@ -75,7 +75,8 @@ class MainModel(nn.Module):
             return x
         #x = x.view(x.size(0), -1)
         print('before x.shape: {0};'.format(x.shape))
-        x = x.view(x.size(0), x.size(1))
+        #x = x.view(x.size(0), x.size(1))
+        x = torch.flatten(x, start_dim=1, end_dim=-1)
         print('after x.shape: {0};'.format(x.shape))
         out = []
         out.append(self.classifier(x))
