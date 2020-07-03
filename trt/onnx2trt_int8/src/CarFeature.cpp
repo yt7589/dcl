1435d2860a10855f15f28d623d4e2f67a58dd081
yantao <yt7589@qq.com>
2020-07-03 04:48:05 +0000

    rebase on the server

--- trt/onnx2trt_int8/src/CarFeature.cpp
+++ trt/onnx2trt_int8/src/CarFeature.cpp
@@ -98,16 +98,16 @@ JNADLL void *VehicleFeatureInstance(const string &modelPath, int cardNum,int max
         return NULL;
     }
     samplesCommon::OnnxSampleParams params;
-    params.onnxFileName = modelPath + "dcl_v020.onnx";
+    params.onnxFileName = modelPath + "dcl_pt12.onnx";
     params.inputTensorNames.emplace_back("data");
     params.batchSize = max_batch_size;
     params.outputTensorNames.emplace_back("output");
     params.gpuId = cardNum;
-    params.engineFileName =modelPath+ "dcl_v020_q.trt";
+    params.engineFileName =modelPath+ "dcl_pt12_q.trt";
     params.dataDirs.emplace_back("");
-    params.dataFile = "../models/calib_images_all.txt";
+    params.dataFile = "../models/calib_images.txt";
     params.int8 = true;
-    std::cout<<"CarFeature.VehicleFeatureInstance 1"<<std::endl;
+    std::cout<<"v0.0.1 CarFeature.VehicleFeatureInstance 1"<<std::endl;
     cudaSetDevice(cardNum);
     std::cout<<"CarFeature.VehicleFeatureInstance 2"<<std::endl;
     CarFeatureExtract::OnnxTRT *handler = new CarFeatureExtract::OnnxTRT(params,0);
