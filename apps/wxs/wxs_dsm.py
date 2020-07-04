d52dbe3d8c4353e58bb41f3029ce9d6d55fa7cae
yt7589 <yt7589@qq.com>
2020-07-04 10:42:19 +0800

    add the common statistics between our and bid

--- apps/wxs/wxs_dsm.py
+++ apps/wxs/wxs_dsm.py
@@ -6,47 +6,55 @@ class WxsDsm(object):
 
     @staticmethod
     def know_init_status():
-        bid_brand_set, bid_bmy_set, bid_ggh_set = WxsDsm._get_bid_info()
+        bid_brand_set, bid_bmy_set, bid_vin_set = WxsDsm._get_bid_info()
         print('标书要求：车辆识别码：{0}个；品牌：{1}个；年款：{2}个；'.format(
-            len(bid_ggh_set), len(bid_brand_set), len(bid_bmy_set)
+            len(bid_vin_set), len(bid_brand_set), len(bid_bmy_set)
         ))
-        our_brand_set, our_bmy_set, our_ggh_set = WxsDsm._get_our_info()
+        our_brand_set, our_bmy_set, our_vin_set = WxsDsm._get_our_info()
         print('自有情况：车辆识别码：{0}个；品牌：{1}个；年款：{2}个；'.format(
-            len(our_ggh_set), len(our_brand_set), len(our_bmy_set)
+            len(our_vin_set), len(our_brand_set), len(our_bmy_set)
         ))
         # 统计品牌情况
+        common_brand_set = our_brand_set & bid_brand_set
+        print('我们和标书公共品牌：{0}个'.format(len(common_brand_set)))
         oh_brand_set = our_brand_set - bid_brand_set
-        print('我们有所里没有品牌：{0}个；'.format(len(oh_brand_set)))
+        print('我们有标书没有品牌：{0}个；'.format(len(oh_brand_set)))
         bh_brand_set = bid_brand_set - our_brand_set
         print('标书有我们没有的品牌：{0}个'.format(len(bh_brand_set)))
         all_brand_set = our_brand_set | bid_brand_set
         print('共有品牌：{0}个'.format(len(all_brand_set)))
+        print('******************************************************')
         # 统计年款情况
+        common_bmy_set = our_bmy_set & bid_bmy_set
+        print('我们和标书公共年款：{0}个'.format(len(common_bmy_set)))
         oh_bmy_set = our_bmy_set - bid_bmy_set
         print('我们有标书没有年款：{0}个'.format(len(oh_bmy_set)))
         bh_bmy_set = bid_bmy_set - our_bmy_set
         print('标书有我们没有年款：{0}个'.format(len(bh_bmy_set)))
         all_bmy_set = our_bmy_set | bid_bmy_set
         print('共有年款：{0}个'.format(len(all_bmy_set)))
+        print('******************************************************')
         # 统计车辆识别码情况
-        oh_vin_set = our_ggh_set - bid_ggh_set
+        common_vin_set = our_vin_set & bid_vin_set
+        print('我们和标书共有车辆识别码：{0}个'.format(len(common_vin_set)))
+        oh_vin_set = our_vin_set - bid_vin_set
         print('我们有标书没有车辆识别码：{0}个'.format(len(oh_vin_set)))
-        bh_vin_set = bid_ggh_set - our_ggh_set
+        bh_vin_set = bid_vin_set - our_vin_set
         print('标书有我们没有车辆识别码：{0}个'.format(len(bh_vin_set)))
-        all_vin_set = our_ggh_set | bid_ggh_set
+        all_vin_set = our_vin_set | bid_vin_set
         print('共有车辆识别码：{0}个'.format(len(all_vin_set)))
     @staticmethod
     def _get_our_info():
         print('掌握当前情况')
         brand_set = set()
         bmy_set = set()
-        ggh_set = set()
+        vin_set = set()
         with open('./work/ggh_to_bmy_dict.txt', 'r', encoding='utf-8') as gfd:
             for line in gfd:
                 row = line.strip()
                 arrs0 = row.split(':')
-                ggh_code = arrs0[0]
-                ggh_set.add(ggh_code)
+                vin_code = arrs0[0]
+                vin_set.add(vin_code)
                 bmy = arrs0[1]
                 bmy_set.add(bmy)
                 arrs1 = arrs0[1].split('_')
@@ -54,12 +62,12 @@ class WxsDsm(object):
                 brand_set.add('{0}牌'.format(brand_name))
                 model_name = arrs1[1]
                 year_name = arrs1[2]
-        return brand_set, bmy_set, ggh_set
+        return brand_set, bmy_set, vin_set
     @staticmethod
     def _get_bid_info():
         brand_set = set()
         bmy_set = set()
-        ggh_set = set()
+        vin_set = set()
         seq = 0
         with open('./logs/bid_20200708.csv', 'r', encoding='utf-8') as cfd:
             for line in cfd:
@@ -72,9 +80,9 @@ class WxsDsm(object):
                     year_name = arrs0[6]
                     bmy = '{0}_{1}_{2}'.format(brand_name, model_name, year_name)
                     bmy_set.add(bmy)
-                    ggh_code = arrs0[8]
-                    ggh_set.add(ggh_code)
+                    vin_code = arrs0[8]
+                    vin_set.add(vin_code)
                 seq += 1
-        return brand_set, bmy_set, ggh_set
+        return brand_set, bmy_set, vin_set
 
     
