6d6835b9056c7e6fb611c5e765696be6768e65d6
yt7589 <yt7589@qq.com>
2020-07-04 16:05:51 +0800

    store all brands to mongodb

--- apps/wxs/wxs_dsm.py
+++ apps/wxs/wxs_dsm.py
@@ -18,11 +18,11 @@ class WxsDsm(object):
         if 1 == i_debug:
             return
 
-        bid_brand_set, bid_bmy_set, bid_vin_set = WxsDsm._get_bid_info()
+        bid_brand_set, bid_model_set, bid_bmy_set, bid_vin_set, _, _, _ = WxsDsm._get_bid_info()
         print('标书要求：车辆识别码：{0}个；品牌：{1}个；年款：{2}个；'.format(
             len(bid_vin_set), len(bid_brand_set), len(bid_bmy_set)
         ))
-        our_brand_set, our_bmy_set, our_vin_set = WxsDsm._get_our_info()
+        our_brand_set, our_model_set, our_bmy_set, our_vin_set = WxsDsm._get_our_info()
         print('自有情况：车辆识别码：{0}个；品牌：{1}个；年款：{2}个；'.format(
             len(our_vin_set), len(our_brand_set), len(our_bmy_set)
         ))
@@ -66,6 +66,7 @@ class WxsDsm(object):
     def _get_our_info():
         print('掌握当前情况')
         brand_set = set()
+        model_set = set()
         bmy_set = set()
         vin_set = set()
         with open('./work/ggh_to_bmy_dict.txt', 'r', encoding='utf-8') as gfd:
@@ -74,20 +75,24 @@ class WxsDsm(object):
                 arrs0 = row.split(':')
                 vin_code = arrs0[0]
                 vin_set.add(vin_code)
-                raw_bmy = arrs0[1]
-                arrs1 = raw_bmy.split('_')
-                bmy = '{0}牌_{1}_{2}'.format(arrs1[0], arrs1[1], arrs1[2])
-                bmy_set.add(bmy)
                 arrs1 = arrs0[1].split('_')
-                brand_name = arrs1[0]
-                brand_set.add('{0}牌'.format(brand_name))
-                model_name = arrs1[1]
+                brand_name = '{0}牌'.format(arrs1[0])
+                brand_set.add(brand_name)
+                model_name_postfix = arrs1[1]
+                model_name = '{0}_{1}'.format(brand_name, model_name_postfix)
+                model_set.add(model_name)
                 year_name = arrs1[2]
-        return brand_set, bmy_set, vin_set
+                bmy = '{0}_{1}_{2}'.format(brand_name, model_name_postfix, year_name)
+                bmy_set.add(bmy)
+        return brand_set, model_set, bmy_set, vin_set
     @staticmethod
     def _get_bid_info():
         brand_set = set()
+        brand_code_dict = {}
+        model_set = set()
+        model_code_dict = {}
         bmy_set = set()
+        bmy_code_dict = {}
         vin_set = set()
         seq = 0
         with open('./logs/bid_20200708.csv', 'r', encoding='utf-8') as cfd:
@@ -97,14 +102,19 @@ class WxsDsm(object):
                 if seq > 0:
                     brand_name = arrs0[2]
                     brand_set.add(brand_name)
-                    model_name = arrs0[4]
+                    brand_code_dict[brand_name] = arrs0[1]
+                    model_name_postfix = arrs0[4]
+                    model_name = '{0}_{1}'.format(brand_name, model_name_postfix)
+                    model_set.add(model_name)
+                    model_code_dict[model_name] = arrs0[3]
                     year_name = arrs0[6]
-                    bmy = '{0}_{1}_{2}'.format(brand_name, model_name, year_name)
+                    bmy = '{0}_{1}_{2}'.format(brand_name, model_name_postfix, year_name)
                     bmy_set.add(bmy)
+                    bmy_code_dict[bmy] = arrs0[5]
                     vin_code = arrs0[8]
                     vin_set.add(vin_code)
                 seq += 1
-        return brand_set, bmy_set, vin_set
+        return brand_set, model_set, bmy_set, vin_set, brand_code_dict, model_code_dict, bmy_code_dict
 
     @staticmethod
     def write_set_to_file(set_obj, filename):
@@ -114,5 +124,28 @@ class WxsDsm(object):
         with open(filename, 'w+', encoding='utf-8') as wfd:
             for item in lst:
                 wfd.write('{0}\n'.format(item))
+    
+    @staticmethod
+    def initialize_db():
+        bid_brand_set, bid_model_set, bid_bmy_set, bid_vin_set, 
+                    brand_code_dict, model_code_dict, 
+                    bmy_code_dict = WxsDsm._get_bid_info()
+        our_brand_set, our_model_set, our_bmy_set, our_vin_set 
+                    = WxsDsm._get_our_info()
+        brands = our_brand_set | bid_brand_set
+        # 保存品牌信息
+        WxsDsm.store_brands_to_db(brands, brand_code_dict)
+
+    @staticmethod
+    def store_brands_to_db(brands, brand_code_dict):
+        num = 1
+        for brand_name in brands:
+            if brand in brand_code_dict:
+                brand_code = brand_code_dict[brand_name]
+            else:
+                brand_code = 'x{0:04d}'.format(num)
+            CBrand.add_brand(brand_name, brand_code)
+            num += 1
+
 
     
