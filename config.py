56120851fa2d63d6763b74f2917513cf00309236
yt7589 <yt7589@qq.com>
2020-07-06 11:46:21 +0800

    change the train dataset to test dataset

--- config.py
+++ config.py
@@ -99,7 +99,7 @@ class LoadConfig(object):
                                            header=None,\
                                            names=['ImageName', 'label'])
             '''
-            self.train_anno = pd.read_csv(os.path.join(self.anno_root, 'train_ds_v4.txt'),\
+            self.train_anno = pd.read_csv(os.path.join(self.anno_root, 'test_ds_v4.txt'),\
                                            sep="*",\
                                            header=None,\
                                            names=['ImageName', 'label'])
@@ -111,7 +111,7 @@ class LoadConfig(object):
                                            header=None,\
                                            names=['ImageName', 'label'])
             '''
-            self.val_anno = pd.read_csv(os.path.join(self.anno_root, 'test_ds_v4n.txt'),\
+            self.val_anno = pd.read_csv(os.path.join(self.anno_root, 'test_ds_v4.txt'),\
                                            sep="*",\
                                            header=None,\
                                            names=['ImageName', 'label'])
