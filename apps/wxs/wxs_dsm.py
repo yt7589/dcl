d482ca37f5f8ddaf0c3eadf1e598dc1e7ffe6a17
yt7589 <yt7589@qq.com>
2020-07-07 11:30:09 +0800

    test normal case of getting bmy_id from t_vin

--- apps/wxs/wxs_dsm.py
+++ apps/wxs/wxs_dsm.py
@@ -247,7 +247,7 @@ class WxsDsm(object):
 
     @staticmethod
     def exp001():
-        vin_code = '19UYA426xxxxx'
+        vin_code = '19UYA426'
         bmy_id = CBmy.get_bmy_id_by_vin_code(vin_code)
         print('bmy_id={0};'.format(bmy_id))
     
