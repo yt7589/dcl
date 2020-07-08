5641dabb7eaf50e43b2d3a0fd53ba82bddd7a7b0
yt7589 <yt7589@qq.com>
2020-07-08 16:47:42 +0800

    create index on t_vin.vin_code

--- app_main.py
+++ app_main.py
@@ -70,8 +70,10 @@ def main(args):
     MMongoDb._initialize()
     ii = 1
     if 1 == ii:
-        app = WxsApp()
-        app.startup(args)
+        rst = MMongoDb.db.t_vin.create_index([('vin_code', pymongo.ASCENDING)], unique=True)
+        print('index rst: {0};'.format(rst))
+        #app = WxsApp()
+        #app.startup(args)
         return
     print('细粒度图像识别系统')
     mode = MODE_DS_MANAGER #MODE_TRAIN_MONITOR
