547b8710a4ffaa3cfa56ff67ebc2fcde71e3ca35
yt7589 <yt7589@qq.com>
2020-07-08 17:20:11 +0800

    run is_sample_exists

--- apps/wxs/controller/c_sample.py
+++ apps/wxs/controller/c_sample.py
@@ -8,13 +8,10 @@ class CSample(object):
 
     @staticmethod
     def add_sample(img_file, vin_id, bmy_id):
-        #MSample.is_sample_exists(img_file)
-        iii = 1
-        if 1 == iii:
-            return 0
         if MSample.is_sample_exists(img_file):
-            rec = MSample.get_sample_by_img_file(img_file)
-            return rec['sample_id']
+            #rec = MSample.get_sample_by_img_file(img_file)
+            return 0 #rec['sample_id']
+        print('#####################################################################')
         sample_id = MPkGenerator.get_pk('sample_id')
         sample_vo = {
             'sample_id': sample_id,
