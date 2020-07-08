7998dc0ccb3ec868e9224c9b8b1c9d94a79bab54
yt7589 <yt7589@qq.com>
2020-07-08 16:54:03 +0800

    restore all the database operation after index

--- apps/wxs/wxs_dsm.py
+++ apps/wxs/wxs_dsm.py
@@ -273,7 +273,6 @@ class WxsDsm(object):
                         arrs2 = raw_vin_code.split('#')
                         vin_code = arrs2[0]
                         bmy_id, vin_id = CBmy.get_bmy_id_by_vin_code(vin_code)
-                        '''
                         if bmy_id < 0:
                             n1 += 1
                             bmy_id, vin_id = CBmy.get_bmy_id_by_prefix_vin_code(vin_code)
@@ -284,7 +283,6 @@ class WxsDsm(object):
                             n3 += 1
                             #wfd.write('############## {0}\n'.format(vin_code))
                             error_vins.append(vin_code)
-                        '''
                         end_time = datetime.datetime.now()
                         batch_run_time += (end_time - start_time).total_seconds()
                         WxsDsm.opr_num += 1
