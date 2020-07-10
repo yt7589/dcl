0f7c95cd501839966350064177dbead369bd2f45
yt7589 <yt7589@qq.com>
2020-07-10 11:27:09 +0800

    deal with we_had_bid_no brand list v1

--- apps/wxs/wxs_dsm.py
+++ apps/wxs/wxs_dsm.py
@@ -491,6 +491,9 @@ class WxsDsm(object):
 
     @staticmethod
     def exp001():
+        vin_bmy_id_dict = CBmy.get_vin_bmy_id_dict()
+        WxsDsm.g_bmy_id_bmy_name_dict = CBmy.get_bmy_id_bmy_name_dict()
+        WxsDsm.g_vin_bmy_id_dict = CBmy.get_vin_bmy_id_dict()
         wb_brand_dict = {}
         we_had_bid_no = []
         with open('./logs/wb_brand.txt', 'r', encoding='utf-8') as wb_fd:
@@ -498,12 +501,44 @@ class WxsDsm(object):
                 line = line.strip()
                 we_had_bid_no.append(line)
         for brand_name in we_had_bid_no:
-            print(brand_name)
+            base_path = Path('/media/zjkj/work/fgvc_dataset/raw/{0}'.format(brand_name[:-1]))
+            is_break = False
+            for model_obj in base_path.iterdir():
+                for year_obj in model_obj.iterdir():
+                    for item_obj in year_obj.iterdir():
+                        item_name = str(item_obj)
+                        if not sub_obj.is_dir() and filename.endswith(
+                                    ('jpg','png','jpeg','bmp')): # 忽略其下目录
+                            arrs0 = item_name.split('/')
+                            arrs1 = arrs0[-1].split('#')
+                            vin_code = arrs1[0]
+                            if vin_code in vin_bmy_id_dict:
+                                bmy_id = vin_bmy_id_dict[vin_code]
+                            elif vin_code[:8] in vin_bmy_id_dict:
+                                bmy_id = vin_bmy_id_dict[vin_code[:8]]
+                            else:
+                                #wfd.write('############## {0}\n'.format(vin_code))
+                                bmy_id = -1
+                            print('正在处理：{0};  {1}'.format(item_name, bmy_id))
+                            if bmy_id > 0:
+                                bmy_name = WxsDsm.g_bmy_id_bmy_name_dict[bmy_id]
+                                arrsn = bmy_name.split('-')
+                                brand_name1 = arrsn[0]
+                                if brand_name not in wb_brand_dict:
+                                    wb_brand_dict[brand_name] = brand_name1
+                                is_break = True
+                                break # break item
+                    if is_break:
+                        break # break year
+                if is_break:
+                    break # break model
+        for k, v in wb_brand_dict.items():
+            print('### {0}: {1};'.format(k, v))
         '''
         
         is_break = False
         for brand_name in we_had_bid_no:
-            base_path = Path('/media/zjkj/work/fgvc_dataset/raw/{0}'.format(brand_name[:-1]))
+            
             is_break = False
             for model_obj in base_path.iterdir():
                 for year_obj in model_obj.iterdir():
