d81f40fb663a6a687128e37d42ea2f1630f73b55
yt7589 <yt7589@qq.com>
2020-07-14 18:05:57 +0800

    add debug log 001

--- utils/eval_model.py
+++ utils/eval_model.py
@@ -76,6 +76,7 @@ def eval_turn(Config, model, data_loader, val_version, epoch_num, log_file):
             pred_size = top3_pos[:, 0].shape[0]
             batch_brand_correct = 0
             for idx in range(pred_size):
+                print('top3_pos[idx][0] = {0}; labels[idx]={1};'.format(top3_pos[idx][0], labels[idx]))
                 pred_brand = fgvc_id_brand_dict[top3_pos[idx][0]]
                 gt_brand = fgvc_id_brand_dict[labels[idx]]
                 if pred_brand == gt_brand:
