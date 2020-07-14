a1f877b59d7acc2816f5929e3bab994ffe11a77e
yt7589 <yt7589@qq.com>
2020-07-14 11:41:49 +0800

    print the size in transforms.Resize to see whether it is aspect reserve

--- transforms/transforms.py
+++ transforms/transforms.py
@@ -188,7 +188,36 @@ class Resize(object):
         Returns:
             PIL Image: Rescaled image.
         """
+        print('transform.Resize: {0}; ***********************'.format(self.size))
         return F.resize(img, self.size, self.interpolation)
+
+    def pre_process(self, image, scale, meta=None):
+        '''
+        示例程序
+        height, width = image.shape[0:2]
+        new_height = int(height * scale) # 原图高
+        new_width  = int(width * scale) # 原图宽
+        
+        inp_height, inp_width = 224, 224
+        c = np.array([new_width / 2., new_height / 2.], dtype=np.float32) # 中心点
+        s = max(height, width) * 1.0 # 找长边
+
+        trans_input = get_affine_transform(c, s, 0, [inp_width, inp_height])
+        resized_image = cv2.resize(image, (new_width, new_height))
+        inp_image0 = cv2.warpAffine(
+        resized_image, trans_input, (inp_width, inp_height),
+        flags=cv2.INTER_LINEAR)
+        inp_image = ((inp_image0 / 255. - mean) / std).astype(np.float32)
+
+        images = inp_image.transpose(2, 0, 1).reshape(1, 3, inp_height, inp_width)
+        images = torch.from_numpy(images)
+        meta = {'c': c, 's': s, 
+                'out_height': inp_height // 4, 
+                'out_width': inp_width // 4}
+        print(images.shape)
+        return images, meta, inp_image0
+        '''
+        return None
 
     def __repr__(self):
         interpolate_str = _pil_interpolation_to_str[self.interpolation]
