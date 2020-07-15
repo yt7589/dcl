8a2fbf193eb01d8c51d1123b1d69b330e9ed7305
yt7589 <yt7589@qq.com>
2020-07-15 11:27:58 +0800

    generate brand test dataset with out create brand dict

--- apps/wxs/wxs_dsm.py
+++ apps/wxs/wxs_dsm.py
@@ -746,7 +746,7 @@ class WxsDsm(object):
         return fgvc_id_brand_dict
 
     @staticmethod
-    def convert_to_brand_ds(bmy_ds_file, brand_ds_file):
+    def convert_to_brand_ds(bmy_ds_file, brand_ds_file, mode=0):
         '''
         将年款数据集转换为品牌数据集
         参数：
@@ -775,14 +775,15 @@ class WxsDsm(object):
                         idx += 1
                     brand_id = brand_name_brand_id_dict[brand_name]
                     bfd.write('{0}*{1}\n'.format(img_file, brand_id))
-        with open('./logs/bid_brands_dict.txt', 'w+', encoding='utf-8') as fd:
-            for k, v in brand_id_brand_name_dict.items():
-                fd.write('{0}:{1}\n'.format(k, v))
+        if 1 == mode:
+            with open('./logs/bid_brands_dict.txt', 'w+', encoding='utf-8') as fd:
+                for k, v in brand_id_brand_name_dict.items():
+                    fd.write('{0}:{1}\n'.format(k, v))
         return len(brand_set)
 
     @staticmethod
     def exp001():
         #WxsDsm.get_simplified_bmys()
         #WxsDsm.get_fgvc_id_brand_dict()
-        brand_num = WxsDsm.convert_to_brand_ds('./logs/bid_train_ds.txt', './logs/bid_brand_train_ds.txt')
+        brand_num = WxsDsm.convert_to_brand_ds('./logs/bid_test_ds.txt', './logs/bid_brand_test_ds.txt', mode=1)
         print('品牌种类：{0};'.format(brand_num))
