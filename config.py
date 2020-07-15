01b5cfafeea0731761c866c8f640c68a2d0461e6
yt7589 <yt7589@qq.com>
2020-07-15 16:01:48 +0800

    add brand label to config.LoadConfig

--- config.py
+++ config.py
@@ -100,10 +100,10 @@ class LoadConfig(object):
                                            header=None,\
                                            names=['ImageName', 'label'])
             '''
-            self.train_anno = pd.read_csv(os.path.join(self.anno_root, 'bid_train_ds.txt'),\
+            self.train_anno = pd.read_csv(os.path.join(self.anno_root, 'bid_brand_train_ds.txt'),\
                                            sep="*",\
                                            header=None,\
-                                           names=['ImageName', 'label'])
+                                           names=['ImageName', 'label', 'brand_label'])
 
         if 'val' in get_list:
             '''
@@ -112,10 +112,10 @@ class LoadConfig(object):
                                            header=None,\
                                            names=['ImageName', 'label'])
             '''
-            self.val_anno = pd.read_csv(os.path.join(self.anno_root, 'bid_test_ds.txt'),\
+            self.val_anno = pd.read_csv(os.path.join(self.anno_root, 'bid_brand_test_ds.txt'),\
                                            sep="*",\
                                            header=None,\
-                                           names=['ImageName', 'label'])
+                                           names=['ImageName', 'label', 'brand_label'])
 
         if 'test' in get_list:
             '''
@@ -124,10 +124,10 @@ class LoadConfig(object):
                                            header=None,\
                                            names=['ImageName', 'label'])
             '''
-            self.test_anno = pd.read_csv(os.path.join(self.anno_root, 'bid_test_ds.txt'),\
+            self.test_anno = pd.read_csv(os.path.join(self.anno_root, 'bid_brand_test_ds.txt'),\
                                            sep="*",\
                                            header=None,\
-                                           names=['ImageName', 'label'])
+                                           names=['ImageName', 'label', 'brand_label'])
 
         self.swap_num = args.swap_num
 
