03e4cb9675ffbd0b7db122489b910957b0a55811
yt7589 <yt7589@qq.com>
2020-08-04 09:44:08 +0800

    fix BUG20200804003

--- apps/wxs/wxs_utils.py
+++ apps/wxs/wxs_utils.py
@@ -12,18 +12,21 @@ class WxsUtils(object):
         base_path = Path('/media/zjkj/work/yantao/fgvc/dcl/net_model/training_descibe_8223_CUB')
         x = []
         y = []
+        data = []
         for file_obj in base_path.iterdir():
             full_path = str(file_obj)
             arrs0 = full_path.split('/')
             log_file = arrs0[-1]
             arrs1 = log_file.split('_')
-            x.append(int(arrs1[2]))
-            y.append(float(arrs1[3]))
+            data.append((int(arrs1[2]), float(arrs1[3])))
         # 绘制测试集上精度变化曲线
-        x.sort()
+        data.sort(key=lambda x:x[0])
+        print('data: {0};'.format(data))
+        for di in data:
+            x.append(di[0])
+            y.append(di[1])
         x = np.array(x)
         print('x: {0};'.format(x))
-        y.sort()
         y = np.array(y)
         print('y: {0};'.format(y))
         fig, ax = plt.subplots()
