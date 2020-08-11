069f4ff09427b6d82d42fb18c151f0ff30bb12dd
yantao <yt7589@qq.com>
2020-08-11 21:00:05 +0800

    the full function test

--- trt/common/exp.cpp
+++ trt/common/exp.cpp
@@ -4,8 +4,13 @@
 
 const int BRAND_NUM = 3;
 const int BMY_NUM = 5;
-// batchSize = 2
 std::vector<std::vector<int>> brandBmys = std::vector<std::vector<int>>{
+    {0, 1},
+    {1, 2, 3},
+    {4}
+};
+// batchSize = 2
+std::vector<std::vector<int>> brandBmys0 = std::vector<std::vector<int>>{
         {0, 34, 1159, 1163, 1171},
         {1, 2, 3, 4, 5, 6, 7, 24, 148, 149, 150, 151, 154, 155, 1148, 1149, 1150, 1151, 1152, 1153, 1154, 1158},
         {8, 9, 10, 11, 30, 31, 32, 75, 81, 87, 94, 99, 274, 316, 320, 357, 459, 460, 461, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 475, 476, 477, 478, 479, 480, 481, 482, 483, 484, 485, 486, 492, 493, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511, 512, 515, 518, 522, 1541, 1615, 1616},
@@ -183,30 +188,38 @@ int main()
 {
     // prepare data
     std::vector<std::vector<float>> net_outputs = std::vector<std::vector<float>>{};
+    std::vector<std::vector<float>> out_results = std::vector<std::vector<float>>{};
     std::vector<float> brand_outputs = std::vector<float>{0.1, 0.7, 0.2, 0.05, 0.05, 0.9};
     net_outputs.emplace_back(brand_outputs);
     std::vector<float> bmy_outputs = std::vector<float>{0.1, 0.1, 0.6, 0.1, 0.1, 0.2, 0.2, 0.05, 0.05, 0.5};
     net_outputs.emplace_back(bmy_outputs);
+
+
+
     //
     for (int in=0; in < net_outputs[0].size() / BRAND_NUM; ++in)
     {
         auto maxPositionBrand = std::max_element(net_outputs[0].begin() + 
                     in * BRAND_NUM, net_outputs[0].begin() + (in + 1) * BRAND_NUM);
         auto indexBrand = maxPositionBrand - (net_outputs[0].begin() + in * BRAND_NUM);
-        std::cout<<"Brand: in="<<in<<"; max="<<*maxPositionBrand<<"; idx="<<indexBrand<<std::endl;
+        //std::cout<<"Brand: in="<<in<<"; max="<<*maxPositionBrand<<"; idx="<<indexBrand<<std::endl;
         std::vector<int> bmyIdxs = brandBmys[indexBrand];
         float maxBmyVal = -0.1f;
         int maxBmyIdx = -1;
         for (int bmyIdx=0; bmyIdx<bmyIdxs.size(); bmyIdx++)
         {
-            std::cout<<bmyIdxs[bmyIdx]<<std::endl;
+            // std::cout<<bmyIdxs[bmyIdx]<<std::endl;
             if (net_outputs[1][in*BMY_NUM + bmyIdxs[bmyIdx]] > maxBmyVal)
             {
                 maxBmyIdx = bmyIdxs[bmyIdx];
                 maxBmyVal = net_outputs[1][in*BMY_NUM + bmyIdxs[bmyIdx]];
             }
         }
-        std::cout<<"idx="<<maxBmyIdx<<"; pos="<<maxBmyVal<<";"<<std::endl;
+        //std::cout<<"idx="<<maxBmyIdx<<"; pos="<<maxBmyVal<<";"<<std::endl;
+        out_results.emplace_back(std::vector<float>{(float)maxBmyIdx, maxBmyVal});
     }
+
+
+
     return 0;
 }
