c944d0baaec36973cf806cc4c61cf1b01d581bdf
yantao <yt7589@qq.com>
2020-08-12 09:24:15 +0800

    fix BUG20200812001

--- apps/wxs/controller/c_bmy.py
+++ apps/wxs/controller/c_bmy.py
@@ -193,6 +193,12 @@ class CBmy(object):
             bmy_id = int(bimi['bmy_id'])
             model_id = int(bimi['model_id'])
             model_vo = MModel.get_model_vo_by_id(model_id)
+            bm_vo = {
+                'model_id': model_id,
+                'model_name': model_vo['model_name'],
+                'model_code': model_vo['model_code'],
+                'source_type': model_vo['source_type']
+            }
             bmy_id_bm_vo_dict[bmy_id] = model_vo
         return bmy_id_bm_vo_dict
 
