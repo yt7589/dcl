4cd6796b3cd18421e67ef5726ff0d8c706a33feb
yantao <yt7589@qq.com>
2020-08-18 20:44:54 +0800

    siamese network v0.0.1

--- models/siamese_app.py
+++ models/siamese_app.py
@@ -20,4 +20,17 @@ class SiameseApp(object):
         self.name = 'models.SiameseApp'
 
     def startup(self, args):
-        print('Siamese Network App')
+        print('Siamese Network App v0.0.1')
+
+    def imshow(self, img,text=None,should_save=False):
+        npimg = img.numpy()
+        plt.axis("off")
+        if text:
+            plt.text(75, 8, text, style='italic',fontweight='bold',
+                bbox={'facecolor':'white', 'alpha':0.8, 'pad':10})
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.show()    
+
+    def show_plot(self, iteration,loss):
+        plt.plot(iteration,loss)
+        plt.show()
