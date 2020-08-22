c5bb85b6e379fad28d189df599c400e66170ba4b
yantao <yt7589@qq.com>
2020-08-22 19:59:54 +0800

    get the real image file of wxs tds ok images

--- apps/wxs/wxs_dsm.py
+++ apps/wxs/wxs_dsm.py
@@ -3385,7 +3385,9 @@ function nextImg() {
             full_fn = str(img_obj)
             arrs_a = full_fn.split('-')
             bmy_code = arrs_a[0]
-            img_file = full_fn[len(bmy_code)+1:]
+            raw_img_file = full_fn[len(bmy_code)+1:]
+            arrs_b = raw_img_file.split('/')
+            img_file = arrs_b[-1]
             print('{0}: {1};'.format(bmy_code, img_file))
     
     @staticmethod
