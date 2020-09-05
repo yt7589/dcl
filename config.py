6f66d70bac083bdb11e6d6d663cfb4b7ef0ab7e9
yantao <yt7589@qq.com>
2020-09-05 09:33:01 +0800

    prepare for the new training and testing dataset

--- config.py
+++ config.py
@@ -95,9 +95,9 @@ class LoadConfig(object):
         # annotation file organized as :
         # path/image_name cls_num\n
         # 正式环境
-        train_ds_file = 'bid_brand_train_ds_0904.txt'
-        val_ds_file = 'bid_brand_test_ds_0904.txt'
-        test_ds_file = 'bid_brand_test_ds_0904.txt'
+        train_ds_file = 'bid_brand_train_ds_090501.txt'
+        val_ds_file = 'bid_brand_test_ds_090501.txt'
+        test_ds_file = 'bid_brand_test_ds_090501.txt'
         '''
         # 精度测试
         val_ds_file = 'wxs_brands_ds.txt'
