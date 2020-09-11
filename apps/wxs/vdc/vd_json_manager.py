1dc1cf3ab05c311dd758c606f77b2e42a24fa663
yantao <yt7589@qq.com>
2020-09-11 10:33:08 +0800

    get all image files to 20 text files

--- apps/wxs/vdc/vd_json_manager.py
+++ apps/wxs/vdc/vd_json_manager.py
@@ -399,11 +399,8 @@ class VdJsonManager(object):
                         #img_full_fns.append(full_fn)
                         num += 1
                         ifds[num % txts_num].write('{0}\n'.format(full_fn))
-                        print('idx={0}: {1}; {2};'.format(num % txts_num, full_fn, ifds[num % txts_num]))
-                        if num > 30:
-                            return
-                        #if num % 1000 == 0:
-                        #    print('获取到{0}个文件'.format(num))
+                        if num % 1000 == 0:
+                            print('获取到{0}个文件'.format(num))
         print('共有{0}个文件'.format(num))
         
     @staticmethod
