b9dc6f7fd2f31b5ddabe89926cdcc2f59d66dc3a
yantao <yt7589@qq.com>
2020-09-24 16:01:38 +0800

    call bind_brand_head_bmy_head

--- apps/wxs/wxs_app.py
+++ apps/wxs/wxs_app.py
@@ -36,7 +36,7 @@ class WxsApp(object):
 
     def startup(self, args):
         print('2020年7月无锡所招标应用')
-        mode = WxsApp.RM_CONVERT_TO_BRAND_DS_MAIN
+        mode = WxsApp.RM_BIND_BRAND_HEAD_BMY_HEAD
         if WxsApp.RM_GENERATE_SAMPLES == mode:
             ''' 
             从fgvc_dataset/raw和guochanchezuowan_all目录生成样本列表
