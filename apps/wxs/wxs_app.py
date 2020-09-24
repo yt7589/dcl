25c55ee86eb56e36ba7f378b84316b1cb2e8c00a
yantao <yt7589@qq.com>
2020-09-24 15:56:38 +0800

    call get_simplified_bmys

--- apps/wxs/wxs_app.py
+++ apps/wxs/wxs_app.py
@@ -36,7 +36,7 @@ class WxsApp(object):
 
     def startup(self, args):
         print('2020年7月无锡所招标应用')
-        mode = WxsApp.RM_GET_VD_FAILED_IMAGES
+        mode = WxsApp.RM_GET_SIMPLIFIED_BMYS
         if WxsApp.RM_GENERATE_SAMPLES == mode:
             ''' 
             从fgvc_dataset/raw和guochanchezuowan_all目录生成样本列表
